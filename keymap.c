da2b8037ad7fece9dfa1bdc430edf5f9700793b3
DonjiKong <60264291+DonjiKong@users.noreply.github.com>
2023-03-26 23:24:08 -0700

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -56,16 +56,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 
+enum custom_keycodes {
+  SHIFT_CTRL_PRTSC = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SHIFT_CTRL_PRTSC:
+      if (record->event.pressed) {
+        // when the key is pressed
+        register_code(KC_LSFT);
+        register_code(KC_LCTL);
+        register_code(KC_PSCR);
+        unregister_code(KC_PSCR);
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LSFT);
+      }
+      break;
+  }
+  return true;
+}
+
+
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
           KC_ESC,        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,
-           KC_QUES,       KC_EXLM,  KC_LBRC,  KC_LCBR,  KC_LPRN,  KC_AMPR,  
-           KC_SCLN,       KC_Z,     KC_M,     KC_L,     KC_D,     KC_F, 
+           KC_QUES,       KC_EXLM,  KC_LBRC,  KC_LCBR,  KC_LPRN,  KC_UNDS,  
+           KC_DQUO,       KC_Z,     KC_M,     KC_L,     KC_D,     KC_F, 
            KC_X,          KC_R,     KC_N,     KC_T,     KC_S,     KC_G, 
            KC_ESC,        MO(1),    KC_B,     KC_W,     KC_C,     KC_V, 
-                          KC_DLR,    KC_SLSH,  KC_HASH,  KC_DQUO,  
+                          KC_CIRC,  KC_GRV,   KC_PEQL,  KC_DLR,  
                                   
                                              KC_BSPC, KC_PGUP, 
                                                       KC_LALT, 
@@ -73,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
            
           KC_F9,         KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,   KC_NO,    QK_BOOT, 
-           KC_PEQL,       KC_RPRN,  KC_RCBR,  KC_RBRC,  KC_ASTR,  KC_PLUS,
-           KC_COMM,       KC_U,     KC_O,     KC_Y,     KC_PMNS,  KC_UNDS, 
+           KC_PMNS,       KC_RPRN,  KC_RCBR,  KC_RBRC,  KC_ASTR,  KC_PLUS,
+           KC_COMM,       KC_U,     KC_O,     KC_Y,     KC_SCLN,  KC_QUOT, 
            KC_P,          KC_A,     KC_E,     KC_I,     KC_H,     KC_TAB, 
            KC_BSPC,       KC_K,     KC_J,     KC_DOT,   KC_Q,     KC_COLN,  
-                          KC_QUOT,  KC_GRV,   KC_BSLS,  KC_AT, 
+                          KC_PERC,  KC_HASH,  KC_SLSH,  KC_BSLS, 
                  
                      KC_PGUP, KC_PGDN, 
                      KC_TAB, 
@@ -87,22 +110,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         [1] = LAYOUT(
           KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-           KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-           KC_NO,         KC_NO,    KC_NO,    KC_UP,    KC_NO,    KC_NO, 
+           KC_NO,         KC_NO,    KC_LBRC,  KC_LCBR,  KC_LPRN,  KC_PERC,
+           KC_NO,         KC_NO,    KC_CIRC,  KC_UP,    KC_DLR,   KC_NO, 
            KC_BSPC,       KC_NO,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_NO, 
-           KC_LSFT,       KC_TRNS,  MO(2),    KC_NO,    KC_NO,    KC_NO, 
-                          KC_NO,    KC_NO,    KC_NO,    KC_NO,
+           KC_LSFT,       KC_TRNS,  KC_B,     KC_W,     KC_NO,    KC_NO, 
+                          KC_NO,    KC_NO,    KC_NO,    SHIFT_CTRL_PRTSC,
 
                                                KC_NO, KC_NO,
                                                       KC_NO, 
-                                     KC_LSFT, KC_ENT, KC_NO,
+                                       MO(2), KC_ENT, KC_NO,
 	
           KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, 
-           KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, 
-           KC_COMM,       KC_7,     KC_8,     KC_9,     KC_PMNS,  KC_NO, 
-           KC_CIRC,       KC_4,     KC_5,     KC_6,     KC_PERC,  KC_TAB, 
-           KC_BSPC,       KC_1,     KC_2,     KC_3,     KC_NO,    KC_NO,
-                          KC_QUES,  KC_0,     KC_NO,    KC_NO, 
+           KC_PEQL,       KC_RPRN,  KC_RCBR,  KC_RBRC,  KC_ASTR,  KC_PLUS, 
+           KC_COMM,       KC_7,     KC_8,     KC_9,     KC_SCLN,  KC_QUOT, 
+           KC_AMPR,       KC_4,     KC_5,     KC_6,     KC_AT,    KC_TAB, 
+           KC_BSPC,       KC_1,     KC_2,     KC_3,     KC_NO,    KC_COLN,
+                          KC_DOT,   KC_0,     KC_SLSH,  KC_BSLS, 
 
                       KC_NO, KC_NO, 
                       KC_NO, 
